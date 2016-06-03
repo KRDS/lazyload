5bfbf9815b72cf5c671d5edacc908b81d3e26abc
Ip <ipernet@gmail.com>
2016-06-03 21:12:08 +1200

    Update to match UMD loading of others PxLoader plugins

--- PxLoaderCss.js
+++ PxLoaderCss.js
@@ -1,51 +1,68 @@
-/**
- * PxLoader plugin to load CSS
- */
-function PxLoaderCss(src, tags, priority)
-{
-    var self	=	this,
-        loader	=	null;
-
-    this.tags		=	tags;
-    this.priority	=	priority;
-	
-	this.lazyLoad	=	new CssLoader();
-	
-	var onLoad = function()
+// PxLoader plugin to load CSS
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['pxloader'], function (PxLoader) {
+            return (root.PxLoaderImage = factory(PxLoader));
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('pxloader'));
+    } else {
+        // Browser globals
+        root.PxLoaderImage = factory(root.PxLoader);
+    }
+}(this, function (PxLoader) {
+	function PxLoaderCss(src, tags, priority)
 	{
-        loader.onLoad(self);
-    };
+		var self	=	this,
+			loader	=	null;
 
-    this.start = function(pxLoader)
+		this.tags		=	tags;
+		this.priority	=	priority;
+		
+		this.lazyLoad	=	new CssLoader();
+		
+		var onLoad = function()
+		{
+			loader.onLoad(self);
+		};
+
+		this.start = function(pxLoader)
+		{
+			// we need the loader ref so we can notify upon completion
+			loader = pxLoader;
+
+			self.lazyLoad.css(src, onLoad);
+		};
+
+		// called by PxLoader when it is no longer waiting
+		this.onTimeout = function()
+		{	   		
+			if( ! self.lazyLoad.pending)
+				loader.onLoad(self);
+			else
+				loader.onTimeout(self);
+		};
+
+		// returns a name for the resource that can be used in logging
+		this.getName = function()
+		{
+			return src;
+		};
+	}
+
+	// add a convenience method to PxLoader for adding a font
+	PxLoader.prototype.addCss = function(src, tags, priority)
 	{
-        // we need the loader ref so we can notify upon completion
-        loader = pxLoader;
+		var cssLoader = new PxLoaderCss(src, tags, priority);
+		this.add(cssLoader);
 
-		self.lazyLoad.css(src, onLoad);
-    };
+		// return the Font instance to the caller
+		return cssLoader.lazyLoad;
+	};
 
-    // called by PxLoader when it is no longer waiting
-    this.onTimeout = function()
-	{	   		
-	    if( ! self.lazyLoad.pending)
-            loader.onLoad(self);
-        else
-            loader.onTimeout(self);
-    };
-
-    // returns a name for the resource that can be used in logging
-    this.getName = function()
-	{
-        return src;
-    };
-}
-
-// add a convenience method to PxLoader for adding a font
-PxLoader.prototype.addCss = function(src, tags, priority)
-{
-    var cssLoader = new PxLoaderCss(src, tags, priority);
-    this.add(cssLoader);
-
-    // return the Font instance to the caller
-    return cssLoader.lazyLoad;
-};
+	return PxLoaderCss;
+}));
